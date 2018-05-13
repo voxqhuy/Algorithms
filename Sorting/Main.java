2e4d38f7ff3b0c6fb87bcdee38eadbec66d0de5a
Vo Huy <huyvo@southern.edu>
2018-05-13 01:44:34 -0400

    update

--- Sorting/Main.java
+++ Sorting/Main.java
@@ -6,7 +6,7 @@ public class Main {   // Save as "Hello.java" under "d:\myProject"
 
  		Utils utils = new Utils();
    		// the unsorted array
-   		int[] arr = Utils.makeNumbers(0, 20, 10);
+   		int[] arr = Utils.makeNumbers(0, 20, 100);
    		System.out.println(Arrays.toString(arr));
    		// the sorted array
    		int[] sortedArray;
@@ -16,18 +16,15 @@ public class Main {   // Save as "Hello.java" under "d:\myProject"
    		/* Basic sortings */
     	BasicSorting bs = new BasicSorting();
     	// INSERTION SORT
-    	long startTime = System.nanoTime();
-      	// sortedArray = bs.insertionSort(arr.clone());
-      	System.out.println(Arrays.toString(bs.insertionSort(arr.clone())));
+      	long startTime = System.nanoTime();
+      	sortedArray = bs.insertionSort(arr.clone());
       	long endTime = System.nanoTime();
       	System.out.println(endTime - startTime);
+      	System.out.println(Arrays.toString(sortedArray));
 
-      	long startTime1 = System.nanoTime();
-      	sortedArray = bs.insertionSort(arr.clone());
-      	// System.out.println(Arrays.toString(sortedArray));
-      	long endTime1 = System.nanoTime();
-      	System.out.println(endTime1 - startTime1);
-
-      	System.out.println(Arrays.toString(bs.insertionSort2(arr.clone())));
+      	startTime = System.nanoTime();
+      	sortedArray = bs.insertionSort2(arr.clone());
+      	endTime = System.nanoTime();
+      	System.out.println(endTime - startTime);
    }
 }
